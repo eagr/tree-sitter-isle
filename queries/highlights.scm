3ee9e23d4173d2f8f4ced263f5dc31777b7f7642
Julian Eager <eagr@tutanota.com>
2025-01-07 19:47:06 +0800

    use agnostic capture names
    close #1

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,35 +1,41 @@
 (comment) @comment
-(int) @number
-(const_ident) @constant
 
 (ident) @variable
-((ident) @type (#match? @type "^[A-Z]"))
+((ident) @type
+  (#match? @type "^[A-Z]"))
+
+(const_ident) @constant
+
+(int) @number
+
+[
+  "true"
+  "false"
+] @boolean
 
 [
   "@"
 ] @operator
+
+(wildcard) @keyword
 
 [
   "("
   ")"
 ] @punctuation.bracket
 
-[
-  "true"
-  "false"
-] @constant.builtin
+(pragma op: "pragma" @keyword)
 
-(pragma op: "pragma" @function.builtin)
-
-(type op: "type" @function.builtin)
+(type op: "type" @keyword)
 (type modifier: "extern" @keyword)
 (type modifier: "nodebug" @keyword)
-(type_enum op: "enum" @function.builtin)
-(type_primitive op: "primitive" @function.builtin)
-(variant_field name: (ident) @variable.member)
+(type_enum op: "enum" @keyword)
+(type_primitive op: "primitive" @keyword)
+(enum_variant name: (ident) @constant)
+(variant_field name: (ident) @property)
 (variant_field type: (ty) @type)
 
-(decl op: "decl" @function.builtin
+(decl op: "decl" @keyword
       term: (ident) @function
       ret_type: (ty) @type)
 (decl modifier_pure: "pure" @keyword)
@@ -37,28 +43,28 @@
 (decl modifier_partial: "partial" @keyword)
 (param_types (ty) @type)
 
-(rule op: "rule" @function.builtin)
-(if_let op: "if-let" @function.builtin)
-(if op: "if" @function.builtin)
-(let op: "let" @function.builtin)
+(rule op: "rule" @keyword)
+(if_let op: "if-let" @keyword)
+(if op: "if" @keyword)
+(let op: "let" @keyword)
 
-(extractor op: "extractor" @function.builtin)
-(pattern_and op: "and" @function.builtin)
+(extractor op: "extractor" @keyword)
+(pattern_and op: "and" @keyword)
 
-(extern_const op: "extern" @function.builtin
+(extern_const op: "extern" @keyword
               kind: "const" @keyword
               type: (ty) @type)
-(extern_constructor op: "extern" @function.builtin
+(extern_constructor op: "extern" @keyword
                     kind: "constructor" @keyword
                     term: (ident) @function
                     fn: (ident) @function)
-(extern_extractor op: "extern" @function.builtin
+(extern_extractor op: "extern" @keyword
                   kind: "extractor" @keyword
                   term: (ident) @function
                   fn: (ident) @function)
 (extern_extractor modifier: "infallible" @keyword)
 
-(convert  op: "convert" @function.builtin
+(convert  op: "convert" @keyword
           inner: (ty) @type
           outer: (ty) @type
           term: (ident) @function)
